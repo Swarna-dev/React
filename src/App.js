622932675362a7613fd7f95f4702fcefbf0536f5
Swarna-dev <swarnamaliyaravi@gmail.com>
2021-05-19 16:07:02 +0530

    functions and class

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <hi>React</hi><br></br><code>src/App.js</code> and save to reload.
         </p>
         This is Swarna Coding ReactJs  
         
--- src/index.js
+++ src/index.js
@@ -10,15 +10,41 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
+//to know how variables work in react
 const name = 'SWARNA';
-const element = <h1>Hello, {name}</h1>;
+const element = <h1>Hello, {name} element</h1>;
 debugger;
 ReactDOM.render(
   element,
   document.getElementById('content')
 );
 
+//class
+class Demo{
+  method1(){
+    var a=1;
+    var b=2;
+    var c=a+b;
+    document.getElementById("class").innerHTML=c +" method1";
+  }
+  method2(){
+    var a=10;
+    var b=20;
+    var c=a+b;
+    document.getElementById("class").innerHTML=c +" method2";
+  }
+}
+var demoObj=new Demo();
+demoObj.method1();
+demoObj.method2();
+
+// arrow function
+var a=(x)=>{       //function(){
+    document.getElementById("function").innerHTML=x;
+}
+a("function declaration");
+
+//
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
